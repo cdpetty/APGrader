092c204473d74a95f663904955fa9ff7c739fc49
Clayton Petty <git.notyalc@gmail.com>
2014-02-18 21:21:05 -0600

    removed unwanted files

--- modules/restrict.js
+++
@@ -1,8 +0,0 @@
-/* restrict access without session cookie */
-module.exports = function(){
-  return function(req, res, next){
-    var exceptions = ['/login'];
-    if (req.session.ap_sess ||  exceptions.indexOf(req.url) > -1) next();
-    else res.redirect('/login');
-  };
-}

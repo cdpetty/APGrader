cf48d30956e847d61a8d62628f1713bb8d913f4e
JonasWechsler <jonaswechsler@gmail.com>
2014-02-19 14:06:29 -0600

    fixed searching

--- routes/list_users.js
+++ routes/list_users.js
@@ -10,13 +10,17 @@ module.exports = function (req, res) {
         if (req.query.period) query_object.period = req.query.period;
         if (req.query.grade_level) query_object.grade_level = req.query.grade_level;
         var a = '';
+        console.log(query_object);
         db.users.find(query_object).forEach(function (err, user){
+            console.log("user:",user);
             if (err) res.send('Error finding students: ' + err);
             else{
                 a += JSON.stringify(user);
             }
+            if (!user)
+                res.send(a);
         });
-        res.send(a);
+        
     }
     else{
         res.render('list_users');

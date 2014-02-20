9963a9691525826f7d6c742e2dc9121caa8e572a
Clayton Petty <git.notyalc@gmail.com>
2014-02-19 21:56:38 -0600

    created python code for creating random students, upload file of students to database

--- routes/new_students.js
+++ routes/new_students.js
@@ -1,5 +1,10 @@
 var fs = require('fs'),
-  util = require('../modules/utilities');
+    util = require('../modules/utilities'),
+    databaseUrl = "AP_DB",
+    collections = ["users"],
+    db = require("mongojs").connect(databaseUrl, collections);
+
+const num_user_params = 5;
 
 module.exports = function(req, res) {
   var new__dirname = __dirname.substring(0, __dirname.lastIndexOf('/'));
@@ -8,7 +13,25 @@ module.exports = function(req, res) {
     util.save(req.files.new_users, folder_path, function(err, saved){
       if (err) console.log('Error saving file:', err);
       else{
-        res.send('File saved');
+        fs.readFile(folder_path + '/' + req.files.new_users.name, { encoding: 'utf8' }, function(err, data){
+          var users = data.split('\n');
+          console.log(users);
+          for (var user_index = 0; user_index < users.length; user_index++){
+            var user = users[user_index].split(' ');
+            var user_obj = {};
+            user_obj.first = users[0];
+            user_obj.last = user[1];
+            user_obj.grade_level = user[2];
+            user_obj.teacher = user[3];
+            user_obj.dirname = user[0] + '_' + user[1];
+            user_obj.password = '';
+            user_obj.username = '';
+            db.users.save(user_obj, function(err, saved){
+              if (err) res.send(err);
+              else res.send('Successfully loaded names!');
+            });
+          }
+        });
       }
     });
   }

cdf9447711222a11b7e7e2b8c9c4feb63c625ba2
Clayton Petty <git.notyalc@gmail.com>
2014-02-28 19:10:44 -0600

    login to mongoose

--- routes/list_users.js
+++ routes/list_users.js
@@ -3,13 +3,13 @@ var users = require('../models/users');
 module.exports = function(req,res){
   //if client wants to search users
   if ( req.query.teacher || req.query.period || req.query.grade_level){
-    var query_object = {};
-    if (req.query.teacher) query_object.teacher = req.query.teacher;
-    if (req.query.period) query_object.period = req.query.period;
-    if (req.query.grade_level) query_object.grade_level = req.query.grade_level;
+    var query = {};
+    if (req.query.teacher) query.teacher = req.query.teacher;
+    if (req.query.period) query.period = req.query.period;
+    if (req.query.grade_level) query.grade_level = req.query.grade_level;
     
     var a = '';
-    users.find(query_object, function(err, found){
+    users.find(query, function(err, found){
       found.forEach(function(err, user){
         console.log("user:", user);
         if (err) res.send('Error finding students: ' + err);
--- routes/login.js
+++ routes/login.js
@@ -1,25 +1,25 @@
 /* Login */
+var users = require('../models/users');
 
-module.exports = function (db) {
-  return function(req,res){
-    if(req.body.username && req.body.password){
-      var username = req.body.username.toLowerCase();
-      var password = req.body.password;
-      console.log('Username', username, 'password', password);
-      db.users.findOne({username: username, password: password}, function(err, found){
-        console.log(found);
-        if (found){
-          req.session.user_id = found._id;
-          req.session.username = found.username;
-          req.session.password = found.password;
-          req.session.dirname = found.dirname;
-          res.send('You logged in!');
-        }
-        else{
-          res.redirect('/create-new-user');
-        }
-      });
-    }
-    else res.render('login');
-  };
+module.exports = function (req, res) {
+  //If user is logging in
+  if(req.body.username && req.body.password){
+    var query = {username: req.body.username.toLowerCase(), password: req.body.password}
+    console.log('Username:', username, 'password:', password);
+    
+    users.findOne(query, function(err, found){
+      if (err) res.send(err);
+      if (found){
+        req.session.user_id = found._id;
+        req.session.username = found.username;
+        req.session.password = found.password;
+        req.session.dirname = found.dirname;
+        res.send('You logged in!');
+      }
+      else{
+        res.redirect('/create-new-user');
+      }
+    });
+  }
+  else res.render('login');
 };

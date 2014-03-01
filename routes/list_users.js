0c0af9db86c92f24d652ef4c6b201942eb426733
Clayton Petty <git.notyalc@gmail.com>
2014-02-28 19:01:09 -0600

    migrated list_users to mongoose

--- routes/list_users.js
+++ routes/list_users.js
@@ -1,25 +1,27 @@
-var util = require('../modules/utilities');
+var users = require('../models/users');
 
 module.exports = function(req,res){
-    if ( req.query.teacher || req.query.period || req.query.grade_level){
-      var query_object = {};
-      if (req.query.teacher) query_object.teacher = req.query.teacher;
-      if (req.query.period) query_object.period = req.query.period;
-      if (req.query.grade_level) query_object.grade_level = req.query.grade_level;
-      var a = '';
-      console.log(query_object);
-      db.users.find(query_object).forEach(function (err, user){
-        console.log("user:",user);
+  //if client wants to search users
+  if ( req.query.teacher || req.query.period || req.query.grade_level){
+    var query_object = {};
+    if (req.query.teacher) query_object.teacher = req.query.teacher;
+    if (req.query.period) query_object.period = req.query.period;
+    if (req.query.grade_level) query_object.grade_level = req.query.grade_level;
+    
+    var a = '';
+    users.find(query_object, function(err, found){
+      found.forEach(function(err, user){
+        console.log("user:", user);
         if (err) res.send('Error finding students: ' + err);
         else{
           a += JSON.stringify(user);
         }
+      });
       if (!user)
         res.send(a);
-      });
-
-    }
-    else{
-      res.render('list_users');
-    }
+    });
+  }
+  else{
+    res.render('list_users');
+  }
 };

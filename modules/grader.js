7ac7bdb1725e379fd976f5832fedbb9b16ba6fc2
notyalc <claytondpetty@gmail.com>
2014-05-14 12:46:15 -0500

    working on diff

--- modules/grader.js
+++ modules/grader.js
@@ -1,21 +1,32 @@
 /* Evaluate and run programs */
 //Modules
 var exec = require('child_process').exec,
-  path = require('path');
+  path = require('path'),
+  fs = require('fs');
 
-module.exports.execute = function(filename, dirname, callback){
+module.exports.execute = function(filename, dirname, labname, callback){
   var folder_path = path.join(__dirname, '../storage/' + dirname)
   var type = getLanguage(filename);
-  if (type === 'python') executePython(folder_path, filename, callback);
-  else if (type === 'java') executeJava(folder_path, filename, callback);
-  else if (type === 'cpp') executeCPlusPlus(folder_path, filename, callback);
+  if (type === 'python') executePython(folder_path, filename, labname, callback);
+  else if (type === 'java') executeJava(folder_path, filename, labname, callback);
+  else if (type === 'cpp') executeCPlusPlus(folder_path, filename, labname, callback);
 };  
 
-function diff (err, stdout, stderr, folder_path, callback){
-  fs.writeFile(folder_path, data, function(err){
-    if (err) callback(err);
-    else callback(err, file);
-  });
+function diff (err, stdout, stderr, folder_path, labname, callback){
+  if (err) callback(err, stdout, stderr);
+  else if (stderr) callback(err, stdout, stderr);
+  else {
+    fs.writeFile(path.join(folder_path, 'tmp_diff_file'), stdout, function(err){
+      if (err) callback(err);
+      else {
+        var diff_command = 'diff -b -i ' + path.join(__dirname, '../labs', labname, labname + '.out') + ' ' + path.join(folder_path, 'tmp_diff_file');
+        exec(diff_command, function(diff_err, diff_stdout, diff_stderr){
+          console.log('ERROR RUNNING THE DIFF COMMAND:', err);
+          callback(err, stdout, stderr, diff_stdout);
+        });
+      }
+    });
+  }
 }
 
 function getLanguage(filename){
@@ -25,7 +36,7 @@ function getLanguage(filename){
   else if (filename_split[1] === 'cpp') return 'cpp';
 }
 
-function executeJava(folder_path, filename, callback){
+function executeJava(folder_path, filename, labname, callback){
   var compile_command = 'javac ' + folder_path + '/' + filename;
   exec(compile_command, function(err, stdout, stderr){
     if(err) callback(err);
@@ -34,21 +45,21 @@ function executeJava(folder_path, filename, callback){
       var run_command = 'java -cp ' + path.join(folder_path, rfilename) + ' > ';
       exec(run_command, function(err, stdout, stderr){
         if (err) callback(err);
-        else diff(err, stdout, stderr, folder_path, callback);
+        else diff(err, stdout, stderr, folder_path, labname, callback);
       });
     }
   });
 };
 
-function executePython(folder_path, filename, callback){
+function executePython(folder_path, filename, labname, callback){
   var run_command = 'python ' + folder_path + '/' + filename;
   exec(run_command, function(err, stdout, stderr){
     if (err) callback(err);
-    else diff(err, stdout, stderr, folder_path, callback);
+    else diff(err, stdout, stderr, folder_path, labname, callback);
   });
 }
 
-function executeCPlusPlus(folder_path, filename, callback){
+function executeCPlusPlus(folder_path, filename, labname, callback){
   var compile_command = 'g++ ' + folder_path + '/' + filename + ' -o ' + folder_path + '/a.out';
   exec(compile_command, function(err, stdout, stderr){
     if(err) callback(err);
@@ -56,7 +67,7 @@ function executeCPlusPlus(folder_path, filename, callback){
       var run_command = folder_path + '/./a.out';
       exec(run_command, function(err, stdout, stderr){
         if (err) callback(err);
-        else diff(err, stdout, stderr, folder_path, callback);
+        else diff(err, stdout, stderr, folder_path, labname, callback);
       });
     }
   });
--- routes/upload.js
+++ routes/upload.js
@@ -13,48 +13,48 @@ module.exports = function(req,res){
       if (err) console.log("Error saving file: ", err);
       else{
         console.log("1");
-        grader.execute(req.files.file.name, req.session.dirname,  function(err, stdout, stderr){
-          if (err) console.log("Error running file:", err);
-          else{
-            console.log("2");
-            db.labs.findOne({name:req.body.lab_name}, function(err, foundLab){
-              if (err) res.send(err);
+        db.labs.findOne({name:req.body.lab_name}, function(err, foundLab){
+          if (err) res.send(err);
+          else if (foundLab){
+            grader.execute(req.files.file.name, req.session.dirname, req.body.lab_name, function(err, stdout, stderr, diff_stdout){
+              if (err) console.log("Error running file:", err);
               else{
-                console.log("3");
-                //GET MOSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS
-                db.submissions.findOne({labname: req.body.lab_name, user: req.session._id}, function(err, foundSubmission){
-                  if (err) res.send(err);
-                  else if (foundSubmission){
-                    console.log("4");
-                    db.submissions.update({name: req.body.lab_name, user: req.session._id}, {attempt: foundSubmission.attempt + 1}, function(err, updated){
-                      if (err) res.send(err);
-                      else res.send("Submission updated: <br>" + "STDOUT:" + stdout + "<br>STDERR:" + stderr);
-                    });
-                  }
-                  else{
-                    console.log("5");
-                    new_lab_submission = new db.submissions();
-                    new_lab_submission.labname = req.body.lab_name;
-                    new_lab_submission.stdout = stdout;
-                    new_lab_submission.stderr = stderr;
-                    new_lab_submission.filename = req.files.file.name;
-                    new_lab_submission.user = req.session.user_id;
-                    new_lab_submission.date = new Date();
-                    new_lab_submission.lab_id = foundLab._id;
-                    new_lab_submission.MOS = "";
-                    new_lab_submission.attempt = 1;
-                    new_lab_submission.save(function(err,saved){
-                      if (err) res.send(err);
-                      else{
-                          db.labs.find({}, function(err, found){
-                          res.render('upload', {message: 'Uploaded with output:', output: stdout, labs: found, name: req.session.name});
-                          //res.send("Submission saved: <br> STDOUT:" + stdout + "<br>STDERR:" + stderr);
-                          });
-                      }
-                    });
-                  }
-                });
-              }
+                console.log("2");
+                  console.log("3");
+                  //GET MOSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS
+                  db.submissions.findOne({labname: req.body.lab_name, user: req.session._id}, function(err, foundSubmission){
+                    if (err) res.send(err);
+                    else if (foundSubmission){
+                      console.log("4");
+                      db.submissions.update({name: req.body.lab_name, user: req.session._id}, {attempt: foundSubmission.attempt + 1}, function(err, updated){
+                        if (err) res.send(err);
+                        else res.send("Submission updated: <br>" + "STDOUT:" + stdout + "<br>STDERR:" + stderr);
+                      });
+                    }
+                    else{
+                      console.log("5");
+                      new_lab_submission = new db.submissions();
+                      new_lab_submission.labname = req.body.lab_name;
+                      new_lab_submission.stdout = stdout;
+                      new_lab_submission.stderr = stderr;
+                      new_lab_submission.filename = req.files.file.name;
+                      new_lab_submission.user = req.session.user_id;
+                      new_lab_submission.date = new Date();
+                      new_lab_submission.lab_id = foundLab._id;
+                      new_lab_submission.MOS = "";
+                      new_lab_submission.attempt = 1;
+                      new_lab_submission.save(function(err,saved){
+                        if (err) res.send(err);
+                        else{
+                            db.labs.find({}, function(err, found){
+                            res.render('upload', {message: 'Uploaded with output:', output: stdout, labs: found, name: req.session.name});
+                            //res.send("Submission saved: <br> STDOUT:" + stdout + "<br>STDERR:" + stderr);
+                            });
+                        }
+                      });
+                    }
+                  });
+                }
             });
           }
         });
